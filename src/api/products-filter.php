883e4411a410df685aaf94a3c4366cea926fefc8
Nikita <nikita.smirnovs_2@edu.rtu.lv>
2025-08-06 19:40:19 +0300

    updated: products-filter

--- src/api/products-filter.php
+++ src/api/products-filter.php
@@ -9,6 +9,7 @@ $origin = $_SERVER['HTTP_ORIGIN'] ?? '';
 
 if (in_array($origin, $allowedOrigins)) {
     header("Access-Control-Allow-Origin: $origin");
+    header("Access-Control-Allow-Credentials: true");
 }
 
 header("Access-Control-Allow-Headers: Content-Type, Authorization");
