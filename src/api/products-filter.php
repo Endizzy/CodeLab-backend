29fac0fc2eed6fe0fdcd1e1dc4c8b01cd77e490d
Nikita <nikita.smirnovs_2@edu.rtu.lv>
2025-08-06 19:52:59 +0300

    refactor: all cors headers in index php

--- src/api/products-filter.php
+++ src/api/products-filter.php
@@ -1,25 +1,25 @@
 <?php
-$allowedOrigins = [
-    'http://localhost:3000',
-    'http://127.0.0.1:3000',
-    'https://codelab-frontend-production.up.railway.app'
-];
-
-$origin = $_SERVER['HTTP_ORIGIN'] ?? '';
-
-if (in_array($origin, $allowedOrigins)) {
-    header("Access-Control-Allow-Origin: $origin");
-    header("Access-Control-Allow-Credentials: true");
-}
-
-header("Access-Control-Allow-Headers: Content-Type, Authorization");
-header("Access-Control-Allow-Methods: GET, OPTIONS");
-header("Content-Type: application/json");
-
-if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
-    http_response_code(200);
-    exit();
-}
+//$allowedOrigins = [
+//    'http://localhost:3000',
+//    'http://127.0.0.1:3000',
+//    'https://codelab-frontend-production.up.railway.app'
+//];
+//
+//$origin = $_SERVER['HTTP_ORIGIN'] ?? '';
+//
+//if (in_array($origin, $allowedOrigins)) {
+//    header("Access-Control-Allow-Origin: $origin");
+//    header("Access-Control-Allow-Credentials: true");
+//}
+//
+//header("Access-Control-Allow-Headers: Content-Type, Authorization");
+//header("Access-Control-Allow-Methods: GET, OPTIONS");
+//header("Content-Type: application/json");
+//
+//if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
+//    http_response_code(200);
+//    exit();
+//}
 
 require_once __DIR__ . '/../config/db.php';
 
